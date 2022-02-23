1383911824adc8c1ef35969377c6bddab179fcc4
mukuldutt47 <mukuld@eoxvantage.in>
2022-02-23 13:11:02 +0530

    questions updated

--- set_2.js
+++ set_2.js
@@ -1,34 +1,37 @@
-
-// 1 -> output
 // console.log(1);
 // setTimeout(() => console.log(2));
 // (() => console.log(3))();
 // console.log(4);
 
-//2 -> write a function to verify if a string is a palindrome or not
+//write a function to verify if a string is a palindrome or not
 
-//3 
 // '2' == 2 && !('3' - 3)
 
-//4
 // let a = {name : 'john'}
 // let b = a;
 // b.name = 'Jane';
 // console.log(b.name)
 // console.log(a.name);
 
-// 5 
-// let b = [{c : 10}];
-// b[0].c = 20;
-// let b1 = b;
-// b1[0].c = 30;
-// console.log(b1[0].c)
-// console.log(b[0].c)
+//3 ways to empty an array
+// let arr = [1,2,3,4]
 
-//6
 // for(var i=0;i<5;i++){
 //     setTimeout(() => console.log(i))
 // }
 
-// 7
-// sum(2)(3)(4)() => 9
+// (function(x) {
+//     return (function(y) {
+//         console.log(x);
+//     })(2)
+// })(1);
+
+// var x = 21;
+// var foo = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// foo ();
+
+
+//sum(1)(2)(3) -> 6
